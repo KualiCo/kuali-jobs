fe0f2d3709d8ad7d9ead6f84a8c329e051f5a965
jakis <ariovistus@gmail.com>
2018-06-20 18:26:58 -0600

    Adding docker

---
+++ build-tag-push.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+repo='667650582711.dkr.ecr.us-west-2.amazonaws.com/mkt-jobs'
+build_id=$(date +%s)
+
+function build() {
+  docker build -t kuali-cli .
+}
+
+function tag() {
+  docker build -t "${repo}:latest" .
+  docker build -t "${repo}:${build_id}" .
+}
+
+function push() {
+  echo "Logging in to repository ${repo}"
+  $(aws ecr get-login --no-include-email)
+  docker push "${repo}:latest"
+  docker push "${repo}:${build_id}"
+}
+
+build
+tag
+push

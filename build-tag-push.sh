f3c879ec3ddc7c0f9d8d2d26488b8e86496ee023
jakis <ariovistus@gmail.com>
2018-07-05 10:52:34 -0600

    update for build script

--- build-tag-push.sh
+++ build-tag-push.sh
@@ -4,12 +4,11 @@ repo='667650582711.dkr.ecr.us-west-2.amazonaws.com/mkt-jobs'
 build_id=$(date +%s)
 
 function build() {
-  docker build -t mkt-jobs .
+  docker build -t "${repo}:latest" .
 }
 
 function tag() {
-  docker build -t "${repo}:latest" .
-  docker build -t "${repo}:${build_id}" .
+  docker tag "${repo}:latest" "${repo}:${build_id}"
 }
 
 function push() {

b55271f3b14c0dccf91025800035523860bed935
jakis <ariovistus@gmail.com>
2018-08-07 14:20:48 -0600

    working on buildspec

--- slack_notify.sh
+++ slack_notify.sh
@@ -11,8 +11,12 @@ USERNAME="AWS CodeBuild"
 ICON=":aws_cb:"
 CHANNEL=${SLACK_CHANNEL:-"salt_noisy"}
 REPO=${REPO:-"No repo specified.  Set Variable REPO"}
+if [ $CODEBUILD_BUILD_SUCCEEDING -eq 1 ] ; then
+    COLOR='good'
+else
+    COLOR='danger'
+fi
 TEXT=$1
-COLOR=$2
 
 echo "Using slack channel: $SLACK_CHANNEL"
 echo "Using repo REPO: $REPO"

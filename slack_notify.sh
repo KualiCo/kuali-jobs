d6edec8f1b8f43ac07b94f1b230de84186f1c3e9
jakis <ariovistus@gmail.com>
2018-08-09 13:00:03 -0600

    Adding slack notify

--- slack_notify.sh
+++ slack_notify.sh
@@ -9,8 +9,7 @@ if [ -z $SLACK_CHANNEL ]; then SLACK_CHANNEL='salt_noisy'; fi
 if [ -z $REPO ]; then REPO='NOT FOUND'; fi
 
 # Set message header
-ARRAY_CODEBUILD_BUILD_ID=(${CODEBUILD_BUILD_ID//:/ })
-CODEBUILD_JOB=$(echo "${ARRAY_CODEBUILD_BUILD_ID[0]}")
+CODEBUILD_JOB="$(echo $CODEBUILD_BUILD_ID |cut -d':' -f1)"
 URL="https://us-west-2.console.aws.amazon.com/codebuild/home?region=us-west-2#/builds/${CODEBUILD_BUILD_ID}/view/new"
 MSG="<${URL}|${CODEBUILD_JOB}>"
 

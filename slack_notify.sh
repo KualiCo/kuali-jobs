7e5b54c68ea59e595c6277ff83bf1a69c3f9b3f3
jakis <ariovistus@gmail.com>
2018-08-09 12:22:05 -0600

    updating slack msg to send more descriptive topic

--- slack_notify.sh
+++ slack_notify.sh
@@ -8,6 +8,26 @@ COLOR='good'
 TEXT=$1
 if [ $CODEBUILD_BUILD_SUCCEEDING -ne 1 ]; then COLOR='danger'; fi
 
+
+users=(
+John
+Harry
+Jake
+Scott
+Philis
+)
+
+for u in "${users[@]}"
+do
+  echo "$u is a registered user"
+done
+
+# Set message header
+ARRAY_CODEBUILD_BUILD_ID=(${CODEBUILD_BUILD_ID//:/ })
+CODEBUILD_JOB=$(echo "${ARRAY_CODEBUILD_BUILD_ID[0]}")
+URL="https://us-west-2.console.aws.amazon.com/codebuild/home?region=us-west-2#/builds/${CODEBUILD_BUILD_ID}/view/new"
+MSG="<${URL}|${CODEBUILD_JOB}>"
+
 echo "Using slack channel: $SLACK_CHANNEL"
 echo "Using repo REPO: $REPO"
 echo "Color detected as: $COLOR"
@@ -16,7 +36,7 @@ slack_message="{
   \"username\": \"${USERNAME}\",
   \"icon_emoji\": \"${ICON}\",
   \"channel\": \"${CHANNEL}\",
-  \"text\": \"${REPO}\",
+  \"text\": \"${MSG}\",
   \"mrkdwn\": true,
   \"attachments\":[{
      \"color\":\"${COLOR}\" ,

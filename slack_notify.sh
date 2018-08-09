5cfd3c5ca5a4514e7e916f5018aceb0b54d9b13d
jakis <ariovistus@gmail.com>
2018-08-09 10:59:09 -0600

    adding containers for mkt-jobs

--- slack_notify.sh
+++ slack_notify.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-if [ $CODEBUILD_BUILD_SUCCEEDING -eq 1 ] ; then
-    COLOR='good'
-else
-    COLOR='danger'
-fi
 SLACK_URL="https://hooks.slack.com/services/${SLACK_TOKEN}"
 USERNAME="AWS CodeBuild"
 ICON=":aws_cb:"
 CHANNEL=${SLACK_CHANNEL:-"salt_noisy"}
 REPO=${REPO:-"No repo specified.  Set Variable REPO"}
+COLOR='green'
 TEXT=$1
+if [ $CODEBUILD_BUILD_SUCCEEDING -ne 1 ]; then COLOR='danger'; fi
 
 echo "Using slack channel: $SLACK_CHANNEL"
 echo "Using repo REPO: $REPO"
